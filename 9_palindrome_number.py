70be5082b9969663f21893b3b5effb6ae2c031de
N H <nh@MBP.local>
2025-11-13 18:55:53 +0900

    Solved 9. Palindrome Number, without converting the integer to a string

--- 9_palindrome_number.py
+++ 9_palindrome_number.py
@@ -1,6 +1,18 @@
 def isPalindrome(x: int) -> bool:
-    s = str(x)
-    return s == s[::-1]
+    # ans = str(x)
+    # return ans == ans[::-1]
+    
+    # without converting the integer to a string
+    if x < 0:
+        return False
+    
+    ans = 0
+    num = x
+    while num:
+        ans = (ans * 10) + (num % 10)
+        num //= 10
+    
+    return ans == x
 
 print(isPalindrome(121))
 print(isPalindrome(-121))
